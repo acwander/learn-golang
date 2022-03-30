71f0cde4e21891c40b0af3c48d0d16c3907c2d0d
Alex W <alexcwander@gmail.com>
2022-03-30 11:43:38 -0500

    pointers

--- structs/main.go
+++ structs/main.go
@@ -15,7 +15,7 @@ type person struct {
 
 func main() {
 	jim := person{
-		firstName: "John",
+		firstName: "Jim",
 		lastName:  "Pardi",
 		contactInfo: contactInfo{
 			email:   "jim.pardi@gmail.com",
@@ -23,5 +23,29 @@ func main() {
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	// NOTES:
+	// example data: [0001][person{firstName:"Jim"}]
+	// 0001 is the memory address
+	// person{firstName:"Jim"} is the value
+	// turn address into value with *address
+	// turn value into address with &value
+
+	// assign jimPointer to the memory address of jim
+	jimPointer := &jim
+
+	jimPointer.updateName("Jimmy")
+	jim.print()
+
+}
+
+// Receiver functions
+
+// *person - This is a type description, it means we are working with a pointer to a person
+// *pointerToPerson = This is an operator, it means we want to manupulate the value the pointer is referencing
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
