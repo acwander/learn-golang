19bd24537066fe5652d3c7e389d82c2c6f06c98e
Alex W <alexcwander@gmail.com>
2022-04-05 11:28:04 -0500

    add, delete and loop through map

--- map/main.go
+++ map/main.go
@@ -3,10 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	// 3 ways to declare a map..
+
 	colors := map[string]string{
 		"red":   "#ff000",
 		"green": "4bf745",
+		"white": "#fffff",
 	}
 
-	fmt.Println(colors)
+	// var colors map[string]string
+
+	// colors := make(map[int]string)
+
+	// // add key value pair
+	// colors[10] = "#fffff"
+
+	// // delete a key value pair
+	// delete(colors, 10)
+
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+// print all key value pairs in a map
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
 }
